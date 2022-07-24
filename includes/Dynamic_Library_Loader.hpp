73008b267382c8084244e80c3f993733e2f22210
darkturbo <amatsu-mikaboshi@outlook.fr>
2022-07-24 22:36:54 +0200

    Update : removed unnecessary namspaces, removed unnecessary build folder

--- includes/Dynamic_Library_Loader.hpp
+++ includes/Dynamic_Library_Loader.hpp
@@ -5,86 +5,74 @@
 #include <dlfcn.h>
 #include <memory>
 
-namespace Basic_Loader {
-
-    class Library_Loader {
-        public:
-            Library_Loader(const Library_Loader &) = delete;
-            explicit Library_Loader();
-            ~Library_Loader();
+class Static_Library_Loader {
+    public:
+        Static_Library_Loader(const Static_Library_Loader &) = delete;
+        explicit Static_Library_Loader();
+        ~Static_Library_Loader();
 
             //? Open Dynamic Library File (File.so)
             void *openFile(std::string const &);
 
             //? Load Opened File Into Memory & Access Its Entry Point
-            template <typename T>
-            T loadFile(std::string const &entryPoint) const
-            {
-                return (T)dlsym(_handler, entryPoint.c_str());    //? function Represent The Needed Entry Point
-            }
+        template <typename T>
+        T loadFile(std::string const &entryPoint) const
+        {
+            return (T)dlsym(_handler, entryPoint.c_str()); //? entryPoint Represent The Needed Entry Point
+        }
 
-            //? Close Loaded File
-            int closeFile() const;
+        //? Close Loaded File
+        int closeFile() const;
 
-            //? Log Error From Last _handler Instance
-            char *errorLog() const;
+        //? Log Error From Last _handler Instance
+        char *errorLog() const;
 
-        protected:
-        private:
-            void *_handler = nullptr;
-    };
-
+    protected:
+    private:
+        void *_handler = nullptr;
 };
 
-namespace Type_Creator {
-
-    template <class T>
-    class Type_Allocator {
-        public:
-            typedef std::unique_ptr<T> (*UniqueTypePtr)();
-            typedef std::shared_ptr<T> (*SharedTypePtr)();
-    };
-
+template <class T>
+class Type_Allocator {
+    public:
+        typedef std::unique_ptr<T> (*UniqueTypePtr)();
+        typedef std::shared_ptr<T> (*SharedTypePtr)();
 };
 
-namespace Dynamic_Loader {
+class Dynamic_Library_Loader {
+    //? Entry Point Must Be Set From Your Side Using The extern C Linkage
+    public:
+        //? Dynamic_Library_Loader Types For Dynamic Library Loader
+        Dynamic_Library_Loader(const Dynamic_Library_Loader &) = delete;
+        explicit Dynamic_Library_Loader() {}
+        ~Dynamic_Library_Loader() {}
 
-    class Dynamic_Library_Loader {
-        //? Entry Point Must Be Set From Your Side Using The extern C Linkage
-        public:
-            //? Dynamic_Library_Loader Types For Dynamic Library Loader
-            Dynamic_Library_Loader(const Dynamic_Library_Loader &) = delete;
-            explicit Dynamic_Library_Loader() {}
-            ~Dynamic_Library_Loader() {}
+        template <typename T>
+        using UniqueTypePtr = typename Type_Allocator<T>::UniqueTypePtr;
 
-            template <typename T>
-            using UniqueTypePtr = typename Type_Creator::Type_Allocator<T>::UniqueTypePtr;
+        template <typename T>
+        auto loadUnique(std::string const &file, std::string const &entryPoint)
+        {
+            _libraryInstance.openFile(file);
+            UniqueTypePtr<T> ptr = _libraryInstance.loadFile<UniqueTypePtr<T>>(entryPoint);
+            return ptr();
+        }
 
-            template <typename T>
-            auto loadUnique(std::string const &file, std::string const &entryPoint)
-            {
-                _libraryInstance.openFile(file);
-                UniqueTypePtr<T> ptr = _libraryInstance.loadFile<UniqueTypePtr<T>>(entryPoint);
-                return ptr();
-            }
+        template <typename T>
+        using SharedTypePtr = typename Type_Allocator<T>::SharedTypePtr;
 
-            template <typename T>
-            using SharedTypePtr = typename Type_Creator::Type_Allocator<T>::SharedTypePtr;
+        template <typename T>
+        auto loadShared(std::string const &file, std::string const &entryPoint)
+        {
+            _libraryInstance.openFile(file);
+            SharedTypePtr<T> ptr = _libraryInstance.loadFile<SharedTypePtr<T>>(entryPoint);
+            return ptr();
+        }
 
-            template <typename T>
-            auto loadShared(std::string const &file, std::string const &entryPoint)
-            {
-                _libraryInstance.openFile(file);
-                SharedTypePtr<T> ptr = _libraryInstance.loadFile<SharedTypePtr<T>>(entryPoint);
-                return ptr();
-            }
+        void free(void) const { _libraryInstance.closeFile(); }
 
-            void free(void) const { _libraryInstance.closeFile(); }
-
-        private:
-            Basic_Loader::Library_Loader _libraryInstance;
-    };
-
+    private:
+        Static_Library_Loader _libraryInstance;
 };
 
 #endif /* !DYNAMIC_LIBRARY_LOADER_HPP_ */
--- sources/Dynamic_Library_Loader.cpp
+++ sources/Dynamic_Library_Loader.cpp
@@ -1,11 +1,11 @@
 #include "../includes/Dynamic_Library_Loader.hpp"
 
 //? Dynamic Library Loader (.so)
-Basic_Loader::Library_Loader::Library_Loader() {}
+Static_Library_Loader::Static_Library_Loader() {}
 
-Basic_Loader::Library_Loader::~Library_Loader() {}
+Static_Library_Loader::~Static_Library_Loader() {}
 
-void *Basic_Loader::Library_Loader::openFile(std::string const &file)
+void *Static_Library_Loader::openFile(std::string const &file)
 {
     _handler = dlopen(file.c_str(), RTLD_LAZY);
     if (_handler == nullptr) {
@@ -18,6 +18,6 @@ void *Basic_Loader::Library_Loader::openFile(std::string const &file)
     }
 }
 
-int Basic_Loader::Library_Loader::closeFile(void) const { return dlclose(_handler); }
+int Static_Library_Loader::closeFile(void) const { return dlclose(_handler); }
 
-char *Basic_Loader::Library_Loader::errorLog(void) const { return dlerror(); }
+char *Static_Library_Loader::errorLog(void) const { return dlerror(); }
